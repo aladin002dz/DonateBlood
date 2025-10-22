92e05fe631fa771acb04e32af2660bb47dc0a56e
aladin002dz <mahfoudh.arous@gmail.com>
2025-10-22 10:35:59 +0100

    Remove user signup components and API route for updating user details, streamlining the authentication process and reducing code complexity.

--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUp from "./sign-up-form";
-
-export default function SignUpPage() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50">
-            <SignUp />
-        </div>
-    );
-}
